7ffdf8705ce2a10f8de89e335fd791221eedff26
lequymui2003 <lequymui290603@gmail.com>
2026-03-10 09:21:09 +0700

    add component QuizForm

---
+++ src/components/QuizForm.tsx
@@ -0,0 +1,12 @@
+export const QuizForm = () => {
+  return (
+    <>
+      <div className="w-full h-full flex justify-center py-5">
+        <div className="w-5xl border border-gray-400 rounded-[10px] p-5 flex justify-center">
+          <p className="text-[25px] font-bold text-blue-600">Quiz</p>
+          <div className="flex flex-col"></div>
+        </div>
+      </div>
+    </>
+  );
+};
